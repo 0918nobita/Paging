263fd508a90c753ac6e74a53827a6f38f3f01f74
0918nobita <nobita.0918@gmail.com>
2016-08-15 08:36:18 +0900

    ページリンクに「前」「次」ボタンを追加した

--- index.php
+++ index.php
@@ -38,8 +38,14 @@ try {
 		<li><?php echo htmlspecialchars($comment['comment'],ENT_QUOTES, 'UTF-8'); ?></li>
 	<?php endforeach; ?>
 </ul>
+<?php if ($page > 1) : ?>
+	<a href="?page=<?php echo $page - 1 ?>">前</a>
+<?php endif; ?>
 <?php for ($i = 1; $i <= $totalPages; $i++) : ?>
 	<a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
 <?php endfor; ?>
+<?php if ($page < $totalPages) : ?>
+	<a href="?page=<?php echo $page + 1; ?>">次</a>
+<?php endif; ?>
 </body>
 </html>
