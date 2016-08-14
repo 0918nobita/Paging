1f7ac2b7955088e4a55d767c68b91bca6c68106f
0918nobita <nobita.0918@gmail.com>
2016-08-15 00:53:05 +0900

    コメントを取得して一覧表示するようにした

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
+
+try {
+	$dbh = new PDO('sqlite:database.sqlite3');
+	$sql = 'SELECT * FROM comments';
+	$comments = array();
+	foreach ($dbh->query($sql) as $row) {
+		array_push($comments, $row);
+	}
+} catch(PDOException $e) {
+	echo $e->getMessage();
+	die();
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -9,5 +21,8 @@ error_reporting(E_ALL & ~E_NOTICE);
 </head>
 <body>
 <h1>コメント一覧</h1>
+<?php foreach ($comments as $comment) : ?>
+	<li><?php echo htmlspecialchars($comment['comment'],ENT_QUOTES,'UTF-8'); ?></li>
+<?php endforeach; ?>
 </body>
 </html>
